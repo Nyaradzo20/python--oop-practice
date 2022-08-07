5b41d78e5001e6f24363e98fd6d5ff4a3c29b9d8
Nyaradzo20 <nchinyati20@gmail.com>
2022-08-07 06:10:27 -0700

    **kwargs amd *args

---
+++ args/REAADME.md
@@ -0,0 +1,2 @@
+*args means a function can take as many args as it wants
+*its given to the function as a list
---
+++ args/arg_exa.py
@@ -0,0 +1,4 @@
+def green (name , *args):
+    print("hi", *args)
+green ("foo", "lii", "mitv")
+#**kwargs allows printing of key value pairs
